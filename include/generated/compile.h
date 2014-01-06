d4582e22d0324a82aa329ee72af8248cf01c3109
Matt Svihel <matt.svihel@gmail.com>
2014-01-06 10:07:43 -0600

    Halo 2

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Jan 6 06:41:36 CST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7.4 20131207 (prerelease) (Linaro GCC 4.7-2013.12) "
